3ebd04a5be98bd8b0201ae69fd88654447598d41
Yanwen <xuyanwen2012@gmail.com>
2024-03-27 21:32:09 -0700

    save

--- demo/fine-grained/main.cpp
+++ demo/fine-grained/main.cpp
@@ -28,23 +28,6 @@ void gen_data(const struct pipe* p) {
 BS::thread_pool pool;
 
 // ----------------------------------------------------------------------------
-// Morton
-// ----------------------------------------------------------------------------
-
-void k_morton_func(const glm::vec4* u_points_begin,
-                   const glm::vec4* u_points_end,
-                   morton_t* u_morton_out,  // output
-                   const float min_coord,
-                   const float range) {
-  std::transform(u_points_begin,
-                 u_points_end,
-                 u_morton_out,
-                 [min_coord, range](const glm::vec4& xyz) {
-                   return shared::xyz_to_morton32(xyz, min_coord, range);
-                 });
-}
-
-// ----------------------------------------------------------------------------
 // Sort
 // ----------------------------------------------------------------------------
 
@@ -120,10 +103,121 @@ void k_binning_pass(const size_t tid,
 }
 
 // ----------------------------------------------------------------------------
-// Wrappers (passing )
+// Wrappers (passing all arguments)
 // ----------------------------------------------------------------------------
 
-void wrapper() {}
+void wrapper_morton(pipe* p, int start, int end) {
+  std::transform(
+      p->u_points + start,
+      p->u_points + end,
+      p->u_morton + start,
+      [min_coord = p->min_coord, range = p->range](const glm::vec4& xyz) {
+        return shared::xyz_to_morton32(xyz, min_coord, range);
+      });
+}
+
+void wrapper_bin_pass(pipe* p,
+                      barrier& bar,
+                      int start,
+                      int end,
+                      int bin_pass /* 0..3 */,
+                      int tid,
+                      int n_threads) {
+  if (tid == 0) reset_sort(n_threads);
+  bar.wait();
+  if (bin_pass % 2 == 0) {
+    k_binning_pass(tid,
+                   bar,
+                   p->u_morton + start,
+                   p->u_morton + end,
+                   p->u_morton_alt,
+                   bin_pass * 8);
+  } else {
+    k_binning_pass(tid,
+                   bar,
+                   p->u_morton_alt + start,
+                   p->u_morton_alt + end,
+                   p->u_morton,
+                   bin_pass * 8);
+  }
+}
+
+void wrapper_remove_dups(pipe* p, const int tid) {
+  if (tid == 0) {
+    const auto last = std::unique_copy(
+        p->u_morton, p->u_morton + p->n_input(), p->u_morton_alt);
+    const auto n_unique = std::distance(p->u_morton_alt, last);
+
+    p->set_n_unique(static_cast<int>(n_unique));
+    p->brt.set_n_nodes(n_unique - 1);
+  }
+}
+
+void wrapper_radix_tree(pipe* p, int start, int end) {
+  for (auto i = start; i < end; ++i) {
+    // boundary check
+    if (i < p->n_brt_nodes()) {
+      cpu::process_radix_tree_i(i, p->n_brt_nodes(), p->u_morton_alt, &p->brt);
+    }
+  }
+}
+
+void wrapper_edge_count(pipe* p, int start, int end) {
+  // Kernel E and F (1/2)
+  for (auto i = start; i < end; ++i) {
+    if (i < p->n_brt_nodes()) {
+      shared::process_edge_count_i(
+          i, p->brt.u_prefix_n, p->brt.u_parents, p->u_edge_counts);
+    }
+  }
+}
+
+void wrapper_edge_offset(pipe* p, int tid) {
+  if (tid == 0) {
+    std::partial_sum(p->u_edge_counts,
+                     p->u_edge_counts + p->n_brt_nodes(),
+                     p->u_edge_offsets);
+    const auto n_oct_nodes = p->u_edge_offsets[p->n_brt_nodes() - 1];
+    p->oct.set_n_nodes(n_oct_nodes);
+  }
+}
+
+void wrapper_make_oct_nodes(pipe* p, int start, int end) {
+  for (auto i = start; i < end; ++i) {
+    if (i < p->n_brt_nodes()) {
+      shared::process_oct_node(i,
+                               p->oct.u_children,
+                               p->oct.u_corner,
+                               p->oct.u_cell_size,
+                               p->oct.u_child_node_mask,
+                               p->u_edge_offsets,
+                               p->u_edge_counts,
+                               p->u_morton_alt,  // sorted unique morton
+                               p->brt.u_prefix_n,
+                               p->brt.u_parents,
+                               p->min_coord,
+                               p->range);
+    }
+  }
+}
+
+void wrapper_link_oct_nodes(pipe* p, int start, int end) {
+  for (auto i = start; i < end; ++i) {
+    if (i < p->n_brt_nodes()) {
+      shared::process_link_leaf(i,
+                                p->oct.u_children,
+                                p->oct.u_child_leaf_mask,
+                                p->u_edge_offsets,
+                                p->u_edge_counts,
+                                p->u_morton_alt,  // sorted unique morton
+                                p->brt.u_has_leaf_left,
+                                p->brt.u_has_leaf_right,
+                                p->brt.u_prefix_n,
+                                p->brt.u_parents,
+                                p->brt.u_left_child);
+    }
+  }
+}
 
 // ----------------------------------------------------------------------------
 // CPU only, maximum threads, do everything
@@ -148,144 +242,35 @@ void example_pipe(std::unique_ptr<pipe>& p) {
   for (size_t tid = 0; tid < blks.get_num_blocks(); ++tid) {
     future.push_back(pool.submit_task(
         [start = blks.start(tid), end = blks.end(tid), tid, &bar, p = p.get()] {
-          // Kernel A (1/1)
-          k_morton_func(p->u_points + start,
-                        p->u_points + end,
-                        p->u_morton + start,
-                        p->min_coord,
-                        p->range);
-
-          // Kernel B (1/4)
+          wrapper_morton(p, start, end);
           bar.wait();
 
-          if (tid == 0) reset_sort(desired_n_threads);
-
+          wrapper_bin_pass(p, bar, start, end, 0, tid, desired_n_threads);
+          bar.wait();
+          wrapper_bin_pass(p, bar, start, end, 1, tid, desired_n_threads);
+          bar.wait();
+          wrapper_bin_pass(p, bar, start, end, 2, tid, desired_n_threads);
+          bar.wait();
+          wrapper_bin_pass(p, bar, start, end, 3, tid, desired_n_threads);
           bar.wait();
 
-          k_binning_pass(static_cast<int>(tid),
-                         bar,
-                         p->u_morton + start,
-                         p->u_morton + end,
-                         p->u_morton_alt,
-                         0);
+          wrapper_remove_dups(p, tid);
           bar.wait();
 
-          // (2/4)
-          bar.wait();
-          if (tid == 0) reset_sort(desired_n_threads);
+          wrapper_radix_tree(p, start, end);
           bar.wait();
 
-          k_binning_pass(static_cast<int>(tid),
-                         bar,
-                         p->u_morton_alt + start,
-                         p->u_morton_alt + end,
-                         p->u_morton,
-                         8);
-
+          wrapper_edge_count(p, start, end);
           bar.wait();
 
-          // (3/4)
-          if (tid == 0) reset_sort(desired_n_threads);
+          wrapper_edge_offset(p, tid);
           bar.wait();
 
-          k_binning_pass(static_cast<int>(tid),
-                         bar,
-                         p->u_morton + start,
-                         p->u_morton + end,
-                         p->u_morton_alt,
-                         16);
-
-          // (4/4)
-          bar.wait();
-          if (tid == 0) reset_sort(desired_n_threads);
+          wrapper_make_oct_nodes(p, start, end);
           bar.wait();
 
-          k_binning_pass(static_cast<int>(tid),
-                         bar,
-                         p->u_morton_alt + start,
-                         p->u_morton_alt + end,
-                         p->u_morton,
-                         24);
-          bar.wait();
-
-          // Kernel C (1/1)
-          if (tid == 0) {
-            // remove duplicates (single thread)
-            const auto last = std::unique_copy(
-                p->u_morton, p->u_morton + p->n_input(), p->u_morton_alt);
-            const auto n_unique = std::distance(p->u_morton_alt, last);
-
-            p->set_n_unique(static_cast<int>(n_unique));
-            p->brt.set_n_nodes(n_unique - 1);
-          }
-          bar.wait();
-
-          // Kernel D (1/1) Radix Tree
-          for (auto i = start; i < end; ++i) {
-            // boundary check
-            if (i < p->n_brt_nodes()) {
-              cpu::process_radix_tree_i(
-                  i, p->n_brt_nodes(), p->u_morton_alt, &p->brt);
-            }
-          }
-
-          bar.wait();
-
-          // Kernel E and F (1/2)
-          for (auto i = start; i < end; ++i) {
-            if (i < p->n_brt_nodes()) {
-              shared::process_edge_count_i(
-                  i, p->brt.u_prefix_n, p->brt.u_parents, p->u_edge_counts);
-            }
-          }
-
-          bar.wait();
-          // (2/2)
-          if (tid == 0) {
-            std::partial_sum(p->u_edge_counts,
-                             p->u_edge_counts + p->n_brt_nodes(),
-                             p->u_edge_offsets);
-            const auto n_oct_nodes = p->u_edge_offsets[p->n_brt_nodes() - 1];
-            p->oct.set_n_nodes(n_oct_nodes);
-          }
-          bar.wait();
-
-          // Kernel G (1/2) Octree
-          for (auto i = start; i < end; ++i) {
-            if (i < p->n_brt_nodes()) {
-              shared::process_oct_node(i,
-                                       p->oct.u_children,
-                                       p->oct.u_corner,
-                                       p->oct.u_cell_size,
-                                       p->oct.u_child_node_mask,
-                                       p->u_edge_offsets,
-                                       p->u_edge_counts,
-                                       p->u_morton_alt,  // sorted unique morton
-                                       p->brt.u_prefix_n,
-                                       p->brt.u_parents,
-                                       p->min_coord,
-                                       p->range);
-            }
-          }
-
-          // (2/2)
-          bar.wait();
-          for (auto i = start; i < end; ++i) {
-            if (i < p->n_brt_nodes()) {
-              shared::process_link_leaf(
-                  i,
-                  p->oct.u_children,
-                  p->oct.u_child_leaf_mask,
-                  p->u_edge_offsets,
-                  p->u_edge_counts,
-                  p->u_morton_alt,  // sorted unique morton
-                  p->brt.u_has_leaf_left,
-                  p->brt.u_has_leaf_right,
-                  p->brt.u_prefix_n,
-                  p->brt.u_parents,
-                  p->brt.u_left_child);
-            }
-          }
+          wrapper_link_oct_nodes(p, start, end);
+          // bar.wait();
         }));
   }
 
