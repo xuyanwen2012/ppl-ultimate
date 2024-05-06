85a64c36bca4e97547654a94042357f1d4970d4c
Shaan <shaanmistry03@gmail.com>
2024-05-06 14:04:44 -0700

    save

--- benchmarks/bench_cpu.cpp
+++ benchmarks/bench_cpu.cpp
@@ -9,15 +9,17 @@
 #include "host/dispatcher.hpp"
 
 // Problem size
-// constexpr auto n = 640 * 480;  // ~300k
-constexpr auto n = 1920 * 1080;  // ~2M
+constexpr auto n = 640 * 480;  // ~300k
+//constexpr auto n = 1920 * 1080;  // ~2M
 constexpr auto min_coord = 0.0f;
 constexpr auto range = 1024.0f;
 constexpr auto seed = 114514;
 
+// Max threads
+constexpr auto max_threads = 8;
+
 // Bench mark config
 constexpr auto n_iterations = 5;
-auto max_threads = 1;  // std::thread::hardware_concurrency();
 
 void gen_data(const std::unique_ptr<pipe>& p) {
   std::mt19937 gen(seed);  // NOLINT(cert-msc51-cpp)
@@ -27,178 +29,156 @@ void gen_data(const std::unique_ptr<pipe>& p) {
   });
 }
 
-// ---------------------------------------------------------------------
-static void BM_ComputeMorton(benchmark::State& state) {
+class CPU : public benchmark::Fixture {
+ public:
+  explicit CPU() : p(std::make_unique<pipe>(n, min_coord, range, seed)) {
+    gen_data(p);
+
+    // basically pregenerate the data
+    const auto max_thread = std::thread::hardware_concurrency();
+    cpu::dispatch_ComputeMorton(max_thread, p.get());
+    cpu::dispatch_RadixSort(max_thread, p.get());
+    cpu::dispatch_RemoveDuplicates(max_thread, p.get());
+    cpu::dispatch_BuildRadixTree(1, p.get());
+    cpu::dispatch_EdgeCount(max_thread, p.get());
+    cpu::dispatch_EdgeOffset(max_thread, p.get());
+    // cpu::dispatch_BuildOctree(max_thread, p.get());
+  }
+
+  std::unique_ptr<pipe> p;
+};
+
+// --------------------------------------------------
+// Morton
+// --------------------------------------------------
+
+BENCHMARK_DEFINE_F(CPU, BM_Morton)(benchmark::State& state) {
   const auto n_threads = state.range(0);
-  auto p = std::make_unique<pipe>(n, min_coord, range, seed);
-  gen_data(p);
 
   for (auto _ : state) {
     cpu::dispatch_ComputeMorton(n_threads, p.get());
   }
 }
 
-BENCHMARK(BM_ComputeMorton)
-    ->Unit(benchmark::kMillisecond)
+BENCHMARK_REGISTER_F(CPU, BM_Morton)
     ->RangeMultiplier(2)
     ->Range(1, max_threads)
-    ->Iterations(n_iterations)
-    ->ArgName("Threads");
+    ->ArgName("Threads")
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(n_iterations);
 
-// ---------------------------------------------------------------------
+// --------------------------------------------------
+// Sort
+// --------------------------------------------------
 
-static void BM_RadixSort(benchmark::State& state) {
+BENCHMARK_DEFINE_F(CPU, BM_Sort)(benchmark::State& state) {
   const auto n_threads = state.range(0);
-  auto p = std::make_unique<pipe>(n, min_coord, range, seed);
-  gen_data(p);
-  cpu::dispatch_ComputeMorton(n_threads, p.get());
 
   for (auto _ : state) {
     cpu::dispatch_RadixSort(n_threads, p.get());
   }
 }
 
-BENCHMARK(BM_RadixSort)
-    ->Unit(benchmark::kMillisecond)
+BENCHMARK_REGISTER_F(CPU, BM_Sort)
     ->RangeMultiplier(2)
     ->Range(1, max_threads)
-    ->Iterations(n_iterations)
-    ->ArgName("Threads");
+    ->ArgName("Threads")
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(n_iterations);
 
-// ---------------------------------------------------------------------
+// --------------------------------------------------
+// Unique
+// --------------------------------------------------
 
-static void BM_RemoveDuplicates(benchmark::State& state) {
+BENCHMARK_DEFINE_F(CPU, BM_RemoveDup)(benchmark::State& state) {
   const auto n_threads = state.range(0);
-  auto p = std::make_unique<pipe>(n, min_coord, range, seed);
-  gen_data(p);
-  cpu::dispatch_ComputeMorton(n_threads, p.get());
-  cpu::dispatch_RadixSort(n_threads, p.get());
 
   for (auto _ : state) {
     cpu::dispatch_RemoveDuplicates(n_threads, p.get());
   }
 }
 
-BENCHMARK(BM_RemoveDuplicates)
-    //->Unit(benchmark::kMillisecond)
+BENCHMARK_REGISTER_F(CPU, BM_RemoveDup)
     ->RangeMultiplier(2)
     ->Range(1, max_threads)
-    ->Iterations(n_iterations)
-    ->ArgName("Threads");
+    ->ArgName("Threads")
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(n_iterations);
 
-// ---------------------------------------------------------------------
+// --------------------------------------------------
+// Radix Tree
+// --------------------------------------------------
 
-static void BM_BuildRadixTree(benchmark::State& state) {
+BENCHMARK_DEFINE_F(CPU, BM_RadixTree)(benchmark::State& state) {
   const auto n_threads = state.range(0);
-  auto p = std::make_unique<pipe>(n, min_coord, range, seed);
-  gen_data(p);
-  cpu::dispatch_ComputeMorton(n_threads, p.get());
-  cpu::dispatch_RadixSort(n_threads, p.get());
-  cpu::dispatch_RemoveDuplicates(n_threads, p.get());
 
   for (auto _ : state) {
     cpu::dispatch_BuildRadixTree(n_threads, p.get());
   }
 }
 
-BENCHMARK(BM_BuildRadixTree)
-    ->Unit(benchmark::kMillisecond)
+BENCHMARK_REGISTER_F(CPU, BM_RadixTree)
     ->RangeMultiplier(2)
     ->Range(1, max_threads)
-    ->Iterations(n_iterations)
-    ->ArgName("Threads");
+    ->ArgName("Threads")
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(n_iterations);
+// --------------------------------------------------
+// Edge Count
+// --------------------------------------------------
 
-// ---------------------------------------------------------------------
-
-static void BM_EdgeCount(benchmark::State& state) {
+BENCHMARK_DEFINE_F(CPU, BM_EdgeCount)(benchmark::State& state) {
   const auto n_threads = state.range(0);
-  auto p = std::make_unique<pipe>(n, min_coord, range, seed);
-  gen_data(p);
-  cpu::dispatch_ComputeMorton(n_threads, p.get());
-  cpu::dispatch_RadixSort(n_threads, p.get());
-  cpu::dispatch_RemoveDuplicates(n_threads, p.get());
-  cpu::dispatch_BuildRadixTree(n_threads, p.get());
 
   for (auto _ : state) {
     cpu::dispatch_EdgeCount(n_threads, p.get());
   }
 }
 
-BENCHMARK(BM_EdgeCount)
-    //->Unit(benchmark::kMillisecond)
+BENCHMARK_REGISTER_F(CPU, BM_EdgeCount)
     ->RangeMultiplier(2)
     ->Range(1, max_threads)
-    ->Iterations(n_iterations)
-    ->ArgName("Threads");
+    ->ArgName("Threads")
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(n_iterations);
 
-// ---------------------------------------------------------------------
+// --------------------------------------------------
+// Edge Offset
+// --------------------------------------------------
 
-// static void BM_EdgeOffset(benchmark::State& state) {
-//   const auto n_threads = state.range(0);
-//   auto p = std::make_unique<pipe>(n, min_coord, range, seed);
-//   gen_data(p);
-//   cpu::dispatch_ComputeMorton(n_threads, p.get());
-//   cpu::dispatch_RadixSort(n_threads, p.get());
-//   cpu::dispatch_RemoveDuplicates(n_threads, p.get());
-//   cpu::dispatch_BuildRadixTree(n_threads, p.get());
-//   cpu::dispatch_EdgeCount(n_threads, p.get());
-
-//   for (auto _ : state) {
-//     cpu::dispatch_EdgeOffset(n_threads, p.get());
-//   }
-// }
-
-// BENCHMARK(BM_EdgeOffset)
-//     ->Unit(benchmark::kMillisecond)
-//     ->RangeMultiplier(2)
-//     ->Range(1, max_threads)
-//     ->Iterations(n_iterations)
-//     ->ArgName("Threads");
-
-// ---------------------------------------------------------------------
-
-static void BM_BuildOctree(benchmark::State& state) {
+BENCHMARK_DEFINE_F(CPU, BM_EdgeOffset)(benchmark::State& state) {
   const auto n_threads = state.range(0);
-  auto p = std::make_unique<pipe>(n, min_coord, range, seed);
-  gen_data(p);
-  cpu::dispatch_ComputeMorton(n_threads, p.get());
-  cpu::dispatch_RadixSort(n_threads, p.get());
-  cpu::dispatch_RemoveDuplicates(n_threads, p.get());
-  cpu::dispatch_BuildRadixTree(n_threads, p.get());
-  cpu::dispatch_EdgeCount(n_threads, p.get());
-  // cpu::dispatch_EdgeOffset(n_threads, p.get());
+
+  for (auto _ : state) {
+    cpu::dispatch_EdgeOffset(n_threads, p.get());
+  }
+}
+
+BENCHMARK_REGISTER_F(CPU, BM_EdgeOffset)
+    ->RangeMultiplier(2)
+    ->Range(1, max_threads)
+    ->ArgName("Threads")
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(n_iterations);
+
+// --------------------------------------------------
+// Octree
+// --------------------------------------------------
+
+BENCHMARK_DEFINE_F(CPU, BM_Octree)(benchmark::State& state) {
+  const auto n_threads = state.range(0);
 
   for (auto _ : state) {
     cpu::dispatch_BuildOctree(n_threads, p.get());
   }
 }
 
-BENCHMARK(BM_BuildOctree)
-    //->Unit(benchmark::kMillisecond)
+BENCHMARK_REGISTER_F(CPU, BM_Octree)
+
     ->RangeMultiplier(2)
     ->Range(1, max_threads)
-    ->Iterations(n_iterations)
-    ->ArgName("Threads");
-
-// ---------------------------------------------------------------------
+    ->ArgName("Threads")
+    ->Unit(benchmark::kMillisecond)
+    ->Iterations(n_iterations);
 
 BENCHMARK_MAIN();
-
-// int main(int argc, char** argv) {
-//   // benchmark::Initialize(&argc, argv);
-//   // benchmark::RunSpecifiedBenchmarks(nullptr, nullptr, {});
-//   auto p = std::make_unique<pipe>(n, min_coord, range, seed);
-
-//   gen_data(p);
-
-//   // basically pregenerate the data
-//   const auto max_thread = std::thread::hardware_concurrency();
-//   cpu::dispatch_ComputeMorton(max_thread, p.get());
-//   cpu::dispatch_RadixSort(max_thread, p.get());
-//   cpu::dispatch_RemoveDuplicates(max_thread, p.get());
-//   cpu::dispatch_BuildRadixTree(max_thread, p.get());
-//   cpu::dispatch_EdgeCount(max_thread, p.get());
-//   std::cout << "Starting EdgeOffset" << std::endl;
-//   // cpu::dispatch_EdgeOffset(max_thread, p.get());
-//   cpu::dispatch_BuildOctree(max_thread, p.get());
-// }
--- benchmarks/host_code.cpp
+++ benchmarks/host_code.cpp
@@ -4,19 +4,38 @@
 #include "third-party/BS_thread_pool.hpp"
 namespace cpu {
 
-std::vector<int> cores = {0, 1};
-// initialization function for threads const std::function<void()>& init_task
+std::vector<int> cores = {0};
+std::mutex core_mutex;
+
 void pin_thread() {
+  std::lock_guard<std::mutex> lock(core_mutex);
   cpu_set_t cpuset;
   CPU_ZERO(&cpuset);
-  CPU_SET(5, &cpuset);
+
+  int core = cores.back();
+  cores.pop_back();
+  CPU_SET(core, &cpuset);
   if (sched_setaffinity(0, sizeof(cpu_set_t), &cpuset) != 0) {
     perror("sched_setaffinity failed");
   } else {
-    printf("Thread %u set to CPU %d\n", std::this_thread::get_id(), 5);
+    printf("Thread %u set to CPU %d\n", std::this_thread::get_id(), core);
+  }
+  if (cores.empty()) {
+    cores = {0, 7};
   }
 }
+
+// void pin_thread() {
+//   cpu_set_t cpuset;
+//   CPU_ZERO(&cpuset);
+//   CPU_SET(5, &cpuset);
+//   if (sched_setaffinity(0, sizeof(cpu_set_t), &cpuset) != 0) {
+//     perror("sched_setaffinity failed");
+//   } else {
+//     printf("Thread %u set to CPU %d\n", std::this_thread::get_id(), 5);
+//   }
+// }
 // by default it uses maximum number of threads on the System, great!
-BS::thread_pool pool(pin_thread);
+BS::thread_pool pool;
 
 }  // namespace cpu
